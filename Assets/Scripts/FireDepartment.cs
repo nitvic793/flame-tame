b8f1f692d76588682cd2cbd48ba616ab9033c67a
Nitish Victor <nithishvictor@gmail.com>
2017-10-28 14:48:29 -0400

    Canvas Menu

--- Assets/Scripts/FireDepartment.cs
+++ Assets/Scripts/FireDepartment.cs
@@ -7,11 +7,15 @@ public class FireDepartment : MonoBehaviour
 
     private Renderer buildingRenderer = null;
     private Color defaultColor;
+    public Canvas canvas;
+    private bool canvasVisible = false;
+
     // Use this for initialization
     void Start()
     {
         buildingRenderer = GetComponent<Renderer>();
         defaultColor = buildingRenderer.material.color;
+        canvas.gameObject.SetActive(canvasVisible);
     }
 
     // Update is called once per frame
@@ -23,11 +27,27 @@ public class FireDepartment : MonoBehaviour
     {
         //If your mouse hovers over the GameObject with the script attached, output this message
         buildingRenderer.material.color = Color.green;
+        if (Input.GetMouseButtonDown(0))
+        {
+            canvas.gameObject.SetActive(true);
+        }
     }
 
     void OnMouseExit()
     {
         //The mouse is no longer hovering over the GameObject so output this message each frame
         buildingRenderer.material.color = defaultColor;
+    }
+
+    public void DeployFireTruck()
+    {
+        var fireTruckGameObject = GameObject.Find("FireTruckHandler");
+        var fireTruckHandler = fireTruckGameObject.GetComponent<FireTruckHandler>();
+        fireTruckHandler.DeployFireTruck();
+    }
+
+    public void RemoveCanvas()
+    {
+        canvas.gameObject.SetActive(false);
     }
 }
