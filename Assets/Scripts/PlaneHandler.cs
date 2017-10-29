e59ab9e2f26093f83b490743434b76f548d63d68
Nitish Victor <nithishvictor@gmail.com>
2017-10-29 02:09:18 -0400

    Marker point

--- Assets/Scripts/PlaneHandler.cs
+++ Assets/Scripts/PlaneHandler.cs
@@ -2,28 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaneHandler : MonoBehaviour {
+public class PlaneHandler : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetMouseButtonDown(0))
+    GameObject marker;
+    // Use this for initialization
+    void Start()
+    {
+        marker = GameObject.Find("Marker");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out hit);
+        if (hit.transform != null)
         {
-            RaycastHit hit;
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            Physics.Raycast(ray, out hit);
-            if (hit.transform != null)
-            {
-                if (hit.transform.gameObject.name == "Plane")
-                {
-                    Debug.Log("On Plane");
-                }
-            }
-        }        
+            marker.transform.position = hit.point;
+        }
+
     }
 
 
